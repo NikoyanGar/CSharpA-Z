cef80de0b19fe9f41a113e6d64dc6ee6fa08b260
nikoyangar <nikoyangar@gmail.com>
2024-06-01 11:52:12 +0400

    add new examples on string

---
+++ Lesson_Working_With_String_Data/012_Working_With_String/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _012_Working_With_String
+{
+    //String.Intern() и String.IsInterned()
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello world";
+            string b = a;
+            a = "hello";
+            Console.WriteLine("a:{0}, b:{1}", a, b);
+            Console.WriteLine($"a==b{a == b}");
+            Console.WriteLine(object.ReferenceEquals(a, b));
+
+
+            Console.WriteLine($"(a + \" world\") == b is {(a + " world") == b}");
+
+
+            string hello = "hello";
+            string helloWorld = "hello world";
+            string helloWorld2 = hello + " world";//"hello world"
+
+            Console.WriteLine("helloWorld:{0}, helloWorld2:{1}  helloWorld == helloWorld2:{2}, ReferenceEquals:{3}", helloWorld, helloWorld2,
+                helloWorld == helloWorld2, object.ReferenceEquals(helloWorld, helloWorld2));
+
+
+            string ref1 = String.Intern(helloWorld);
+            string ref2 = String.Intern(helloWorld2);
+
+            Console.WriteLine($"Intern ReferenceEquals:{object.ReferenceEquals(ref1, ref2)}");
+        }
+    }
+}
---
+++ Lesson_Working_With_String_Data/013_Working_With_String/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _013_Working_With_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = new string('t', 3);
+            Console.WriteLine(String.IsInterned(s) ?? "not interned");
+            String.Intern(s);//comment
+            Console.WriteLine(String.IsInterned(s) ?? "not interned");
+            Console.WriteLine(object.ReferenceEquals(String.IsInterned(new string('c', 3)), s));
+        }
+    }
+}
