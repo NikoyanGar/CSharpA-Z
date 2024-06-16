d241b9fab120101acafb6602b0c57e3e65dda91d
nikoyangar <nikoyangar@gmail.com>
2024-06-16 17:02:07 +0400

    add comment on pre and post increment examples

--- Lesson_Operator_Precedence_And_Associativity/008_Operator_Category_Unary_Increments/Program.cs
+++ Lesson_Operator_Precedence_And_Associativity/008_Operator_Category_Unary_Increments/Program.cs
@@ -1,5 +1,9 @@
 ﻿namespace _008_Operator_Category_Unary_Increments
 {
+    //Both ++x (pre-increment) and x++ (post-increment) are unary operators, meaning they operate on a single operand.
+    //Pre-increment(++x) : Increments the value of x by 1, then returns the incremented value.
+    //Post-increment(x++) : Returns the value of x, then increments x by 1.
+
     internal class Program
     {
         static void Main(string[] args)
